22009b9ee93891ddedcac0058ec342a2cbc285e6
Florencia Luz Duarte <florluzduarte@gmail.com>
2025-03-12 15:33:53 -0300

    config: initial setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,28 +1,62 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
+import starlightThemeFlexoki from "starlight-theme-flexoki";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: {
-				github: 'https://github.com/withastro/starlight',
-			},
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+  site: "https://notes.florluzduarte.com",
+  integrations: [
+    starlight({
+      title: "Flor Luz Duarte | Notes",
+      customCss: [
+        "./src/styles/custom-styles.css",
+        "@fontsource/istok-web/400.css",
+        "@fontsource/istok-web/700.css",
+      ],
+      social: {
+        github: "https://github.com/florluzduarte/notes",
+      },
+      plugins: [
+        starlightThemeFlexoki({
+          accentColor: "magenta",
+        }),
+      ],
+      sidebar: [
+        {
+          label: "Start Here",
+          autogenerate: { directory: "start-here", collapsed: true },
+        },
+        {
+          label: "Development",
+          autogenerate: { directory: "development", collapsed: true },
+        },
+        {
+          label: "Design",
+          autogenerate: { directory: "design", collapsed: true },
+        },
+        {
+          label: "Product",
+          autogenerate: { directory: "product", collapsed: true },
+        },
+        {
+          label: "Business",
+          autogenerate: { directory: "business", collapsed: true },
+        },
+        {
+          label: "Books",
+          autogenerate: { directory: "books", collapsed: true },
+        },
+        {
+          label: "Research",
+          autogenerate: { directory: "research", collapsed: true },
+        },
+        {
+          label: "Hobbies",
+          autogenerate: { directory: "hobbies", collapsed: true },
+        },
+      ],
+      credits: true,
+    }),
+  ],
 });
